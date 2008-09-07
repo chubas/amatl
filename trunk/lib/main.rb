385248fa952b6d44d1106983f7af157f6b536f25
CHubas7 <CHubas7@181ccb58-ac40-0410-b3d6-5fb1b1592a93>
2008-09-07 23:50:00 +0000

    Added license headers, and a short description of files. Added RDOC headers too.

--- trunk/lib/main.rb
+++ trunk/lib/main.rb
@@ -1,4 +1,32 @@
+# --
+# Amatl - Desktop widgets for Gnome
+# Copyright (C) 2008 Ruben Medellin <ruben.medellin.c@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# --
+#
+# This file is meant to be the start point of the widget manager, and
+# hence of all the program
+#
+# Author::    Ruben Medellin  (mailto:<ruben.medellin.c@gmail.com>)
+# Copyright:: Copyright (c) 2008
+# License::   GPL v2.0
+
 $AMATL_ROOT = File.expand_path(File.dirname(__FILE__))
+
+#TODO: Add to path common subdirectories, specially for rake files
 require 'manager/manager'
 
 Amatl::Manager.instance
--- trunk/test/proof_of_concept/easy_menus.rb
+++ trunk/test/proof_of_concept/easy_menus.rb
@@ -1,3 +1,28 @@
+# --
+# Amatl - Desktop widgets for Gnome
+# Copyright (C) 2008 Ruben Medellin <ruben.medellin.c@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# --
+#
+# Test for easy menu creation
+#
+# Author::    Ruben Medellin  (mailto:<ruben.medellin.c@gmail.com>)
+# Copyright:: Copyright (c) 2008
+# License::   GPL v2.0
+
 require 'gtk2'
 
 module Amatl
